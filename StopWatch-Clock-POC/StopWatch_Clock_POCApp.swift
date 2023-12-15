1a3e55a04674aae3a7b34c5961dd2470c6e962f3
Harpreet <39591644+HARPREET011992@users.noreply.github.com>
2023-12-15 15:22:44 -0500

    Initial Commit

---
+++ StopWatch-Clock-POC/StopWatch_Clock_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatch_Clock_POCApp.swift
+//  StopWatch-Clock-POC
+//
+//  Created by Harpreet Kaur on 2023-12-15.
+//
+
+import SwiftUI
+
+@main
+struct StopWatch_Clock_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
